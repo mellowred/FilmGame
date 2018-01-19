dc9bd9e905664fd354c5a93360b015e1e3e55e53
Sam Cookson <sbcskyline18@gmail.com>
2018-01-18 18:54:27 -0700

    Update KaboomMic.cs

--- Scripts/KaboomMic.cs
+++ Scripts/KaboomMic.cs
@@ -1,1 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KaboomMic : MonoBehaviour {
+
+	public float detectionRange;
+	public float explodeRange;
+
+	public bool detected = false;
+	public bool inRange = false;
+
+	public Transform player;
+
+	
+	void Update () {
+		Detection();
+	}
+
+	public void Detection() {
+		if(Vector3.Distance(player.position, transform.position) <= detectionRange) {
+			detected = true;
+			Debug.Log("detected");
+		}
+
+		if(detected && Vector3.Distance(player.position, transform.position) <= explodeRange) {
+			Debug.Log("Hurt Player");
+		}
+	}
+}
 
