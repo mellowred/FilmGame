6b170fa347ef7fdf42b0afcd270b523b020b5877
Sam Cookson <sbcskyline18@gmail.com>
2018-01-23 18:44:21 -0700

    Update KaboomMic.cs

--- Scripts/KaboomMic.cs
+++ Scripts/KaboomMic.cs
@@ -1,31 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KaboomMic : MonoBehaviour {
 
 	public float detectionRange;
 	public float explodeRange;
+	public float explodeTime;
 
-	public bool detected = false;
-	public bool inRange = false;
+	public Text countdownText;
+
+	private bool detected = false;
+	private bool inRange = false;
+	private bool explode = false;
 
 	public Transform player;
 
+	void Start() {
+		countdownText = GetComponent<Text>();
+		Detection();
+	}
 	
 	void Update () {
-		Detection();
+
 	}
 
 	public void Detection() {
 		if(Vector3.Distance(player.position, transform.position) <= detectionRange) {
 			detected = true;
-			Debug.Log("detected");
-		}
-
-		if(detected && Vector3.Distance(player.position, transform.position) <= explodeRange) {
-			Debug.Log("Hurt Player");
+			StartCoroutine(Countdown());
 		}
 	}
+
+	public void Explode() {
+ 		if(detected && Vector3.Distance(player.position, transform.position) <= explodeRange && explode == true) {
+			Destroy(player.gameObject);
+		} else {
+			detected = false;
+			explode = false;
+			Detection();
+		}
+	}
+	IEnumerator Countdown() {
+        while (true) {
+            yield return new WaitForSeconds (explodeTime);
+            
+            explode = true;
+            Explode();
+        }
+    }
 }
 
